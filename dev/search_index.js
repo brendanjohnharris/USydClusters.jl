3d233cdfb6b4c21c223f198d71c2062e9237424f
Documenter.jl <documenter@juliadocs.github.io>
2025-07-08 12:54:35 +0000

    build based on 4184e2e

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#USydClusters","page":"Home","title":"USydClusters","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for USydClusters.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+[{"location":"#USydClusters","page":"Home","title":"USydClusters","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for USydClusters.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"#USydClusters.pref_or_env","page":"Home","title":"USydClusters.pref_or_env","text":"Load a preference or environment variable with priority preference -> env -> default\n\n\n\n\n\n","category":"function"}]
 }

70022fc381a09b92e4a2eaee1b6db1975cf0c9a4
brendanjohnharris <brendanjohnharris@gmail.com>
2025-10-21 17:10:13 +1100

    Up

--- src/Physics.jl
+++ src/Physics.jl
@@ -302,7 +302,7 @@ function runscript(script::String;
     end
     queue = isempty(queue) ? queue : "-q $(Base.shell_escape(queue))"
     qsub = "source $(ENV["HOME"])/.bashrc && /usr/physics/pbspro-14.0.1/bin/qsub $(to_string(qsubflags)) $queue $(Base.shell_escape(qsub_file))"
-    qsub_cmd = `ssh headnode "$qsub"`
+    qsub_cmd = `$qsub` # ssh headnode
     jobid = capture_jobid(qsub_cmd)
     return jobid, replace(to_string(logfile), r"\$\{PBS_JOBID\}" => "$jobid.headnode")
 end
@@ -371,7 +371,7 @@ function runscripts(scriptdir::String; # The files should be named 1.jl, 2.jl, e
     end
     queue = isempty(queue) ? queue : "-q $(Base.shell_escape(queue))"
     qsub = "source $(ENV["HOME"])/.bashrc && /usr/physics/pbspro-14.0.1/bin/qsub $(to_string(qsubflags)) $queue $(Base.shell_escape(qsub_file))"
-    qsub_cmd = `ssh headnode "$qsub"`
+    qsub_cmd = `$qsub` # ssh headnode
     @info "Submitting array job with name julia-$ID (logdir: $LOGDIR)"
     jobid = capture_jobid(qsub_cmd)
     return jobid
@@ -380,7 +380,7 @@ end
 function selfdestruct()
     pbsid = split(ENV["PBS_JOBID"], ".") |> first
     @info "Nuking job $pbsid"
-    run(`ssh headnode "/usr/physics/pbspro-14.0.1/bin/qdel $pbsid"`)
+    run(`/usr/physics/pbspro-14.0.1/bin/qdel $pbsid`)
     @info "Nuked job $pbsid." # All going well, this won't run
 end
 end # module

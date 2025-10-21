1b351b3eab8f52ae2443d88853fd2085c785de28
brendanjohnharris <brendanjohnharris@gmail.com>
2025-10-21 17:26:23 +1100

    up

--- src/Physics.jl
+++ src/Physics.jl
@@ -97,10 +97,10 @@ function Distributed.launch(manager::PBSProManager,
         write(f, cmd)
 
         @debug(cmd)
-        _qsub = `/usr/physics/pbspro-14.0.1/bin/qsub $(queue) $(qsubflags)`
+        _qsub = `/opt/pbs/bin/qsub $(queue) $(qsubflags)`
         qsub = "source $(ENV["HOME"])/.bashrc > /dev/null && $(Base.shell_escape(_qsub)) $(Base.shell_escape(f))"
 
-        qsub_cmd = pipeline(`$qsub`) # Sometimes need `ssh headnode '$qsub'`
+        qsub_cmd = pipeline(`sh -c $qsub`) # Sometimes need `ssh headnode '$qsub'`
         @debug qsub_cmd
         out = open(qsub_cmd)
         @debug out
@@ -206,7 +206,7 @@ end
 #         o = remotecall_fetch(f, p, args...; kwargs...)
 #         @debug "Worker $p completed successfully, removing."
 #         # pbsid = Distributed.map_pid_wrkr[p].config.userdata[:job]
-#         # run(`ssh headnode "/usr/physics/pbspro-14.0.1/bin/qdel $pbsid"`)
+#         # run(`ssh headnode "/opt/pbs/bin/qdel $pbsid"`)
 #     catch e
 #         @error "Error in worker $p: $e"
 #         o = nothing
@@ -240,7 +240,7 @@ end
 #     @info "Workers completed successfully, removing."
 #     for p in procs
 #         pbsid = Distributed.map_pid_wrkr[p].config.userdata[:job]
-#         run(`ssh headnode "/usr/physics/pbspro-14.0.1/bin/qdel $pbsid"`)
+#         run(`ssh headnode "/opt/pbs/bin/qdel $pbsid"`)
 #         @info "Job $pbsid removed successfully."
 #     end
 #     return O
@@ -301,8 +301,8 @@ function runscript(script::String;
         write(f, cmd)
     end
     queue = isempty(queue) ? queue : "-q $(Base.shell_escape(queue))"
-    qsub = "source $(ENV["HOME"])/.bashrc && /usr/physics/pbspro-14.0.1/bin/qsub $(to_string(qsubflags)) $queue $(Base.shell_escape(qsub_file))"
-    qsub_cmd = `$qsub` # ssh headnode
+    qsub = "source $(ENV["HOME"])/.bashrc && /opt/pbs/bin/qsub $(to_string(qsubflags)) $queue $(Base.shell_escape(qsub_file))"
+    qsub_cmd = `sh -c $qsub` # ssh headnode
     jobid = capture_jobid(qsub_cmd)
     return jobid, replace(to_string(logfile), r"\$\{PBS_JOBID\}" => "$jobid.headnode")
 end
@@ -370,8 +370,8 @@ function runscripts(scriptdir::String; # The files should be named 1.jl, 2.jl, e
         write(f, cmd)
     end
     queue = isempty(queue) ? queue : "-q $(Base.shell_escape(queue))"
-    qsub = "source $(ENV["HOME"])/.bashrc && /usr/physics/pbspro-14.0.1/bin/qsub $(to_string(qsubflags)) $queue $(Base.shell_escape(qsub_file))"
-    qsub_cmd = `$qsub` # ssh headnode
+    qsub = "source $(ENV["HOME"])/.bashrc && /opt/pbs/bin/qsub $(to_string(qsubflags)) $queue $qsub_file"
+    qsub_cmd = `sh -c $qsub`
     @info "Submitting array job with name julia-$ID (logdir: $LOGDIR)"
     jobid = capture_jobid(qsub_cmd)
     return jobid
@@ -380,7 +380,7 @@ end
 function selfdestruct()
     pbsid = split(ENV["PBS_JOBID"], ".") |> first
     @info "Nuking job $pbsid"
-    run(`/usr/physics/pbspro-14.0.1/bin/qdel $pbsid`)
+    run(`/opt/pbs/bin/qdel $pbsid`)
     @info "Nuked job $pbsid." # All going well, this won't run
 end
 end # module
